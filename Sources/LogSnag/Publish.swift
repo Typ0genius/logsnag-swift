d1b7c19681f64ee4aa6be586c37a4316924176d0
Typ0genius <>
2022-10-10 08:26:27 +0200

    add tags support

--- Sources/LogSnag/Publish.swift
+++ Sources/LogSnag/Publish.swift
@@ -1,14 +1,15 @@
 import Foundation
 
-///Options for publishing LogSnag events
+/// Options for publishing LogSnag events
 public struct PublishOptions: Codable, Equatable {
     let channel: String
     let description: String?
     let event: String
     let icon: String?
     let notify: Bool?
+    let tags: [String: TagValueTypes]?
     var project: String?
-    
+
     /// Creates a `PublishOptions` object to send to LogSnag
     /// - Parameters:
     ///   - channel: Channel name
@@ -16,17 +17,38 @@ public struct PublishOptions: Codable, Equatable {
     ///   - description: Event description (default `nil`)
     ///   - icon: Event icon (emoji)  (default `nil`)
     ///   - notify: Send push notification  (default `nil`)
+    ///   - tags: Event tags  (default `nil`)
     public init(
         channel: String,
         event: String,
         description: String? = nil,
         icon: String? = nil,
-        notify: Bool? = nil
+        notify: Bool? = nil,
+        tags: [String: TagValueTypes]? = nil
     ) {
         self.channel = channel
         self.event = event
         self.description = description
         self.icon = icon
         self.notify = notify
+        self.tags = tags
+    }
+}
+
+public enum TagValueTypes: Codable, Equatable {
+    case bool(Bool)
+    case integer(Int)
+    case string(String)
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .string(let value):
+            try container.encode(value)
+        case .integer(let value):
+            try container.encode(value)
+        case .bool(let value):
+            try container.encode(value)
+        }
     }
 }
